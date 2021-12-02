43dc8f47694d8144fa8977c903443b845149e898
nguyenhuyennd <nguyenhuyennd1211@gmail.com>
2021-12-02 09:25:12 +0700

    test cypress

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,5 +1,6 @@
 describe('My First Test', () => {
     it('Visits the Kitchen Sink', () => {
         cy.visit('https://nextjs-day1-frontend-v3.vercel.app')
+        cy.get('#HomePage').contains('Day la trang chu')
     })
 })
